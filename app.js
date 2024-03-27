80b7e3d85e32d5c9b2f5139ef24e5e15de325a46
pratyushsingha <pratyushsingha83@gmail.com>
2024-03-27 17:50:15 +0530

    fix: url schema fixed for url stats and dns config fixed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import requestIp from "request-ip";
 import cookieParser from "cookie-parser";
 import rateLimit from "express-rate-limit";
+import  Useragent  from "express-useragent";
 
 const app = express();
 app.use(
@@ -36,6 +37,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.use(Useragent.express());
 
 import urlRouter from "./src/routes/url.route.js";
 import linkRouter from "./src/routes/link.route.js";
